7a389adb0ca4b9ca9a4f66c09921bf1e84f3f3ec
Carl Bolstad <carl@trover.com>
2013-05-25 11:28:11 -0700

    get squares into game

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -1,18 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class GameOfLife 
 {
 	private int iterations;
 	private int width;
 	private int height;
+	private ArrayList <Square>squares;
 	
-	public GameOfLife(int iterations, int width, int height)
+	public GameOfLife(int iterations, int width, int height, ArrayList<Square> squares)
 	{
 		this.iterations = iterations;
 		this.width = width;
 		this.height = height;
+		this.squares = squares;
 	}
 	
 	public void run()
@@ -29,14 +32,15 @@ public class GameOfLife
 		//		<EOF>
 		
 		int iterations = 0, width = 0, height = 0;
+		//System.out.print("Enter Game of Life configuration: \n");
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		try 
 		{
 			iterations = Integer.parseInt(br.readLine());
-		} catch (IOException ioe) {
-		     System.out.println("IO error trying to read number of iterations.");
+		} catch (Exception ioe) {
+		     System.out.println("error trying to read number of iterations.");
 		     System.exit(1);
 		}
 		
@@ -44,17 +48,39 @@ public class GameOfLife
 		{
 			String [] widthHeight = br.readLine().split(" ");
 			if(widthHeight.length != 2)
-				throw new IOException("expecting two values in second line representing width and height separated by a space");
+				throw new Exception("expecting two values in second line representing width and height separated by a space");
 			
 			width = Integer.parseInt(widthHeight[0]);
 			height = Integer.parseInt(widthHeight[1]);
 			
-		} catch (IOException ioe) {
-		     System.out.println("IO error trying to read number of iterations.");
+		} catch (Exception ioe) {
+		     System.out.println("error trying to read number of iterations.");
 		     System.exit(1);
 		}
 		
-		GameOfLife game = new GameOfLife(iterations, width, height);
+		ArrayList<Square> squares = new ArrayList<Square>();
+		
+		for(int i = 0; i < height; i++)
+		{
+
+			String[] squaresStrings = null;
+			try {
+				squaresStrings = br.readLine().split(" ");
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(squaresStrings != null)
+			{
+				for(int j = 0; j < width; j++)
+				{
+					Square square = new Square(Integer.parseInt(squaresStrings[j]));
+					squares.add(square);
+				}
+			}
+		}
+		
+		GameOfLife game = new GameOfLife(iterations, width, height, squares);
 		game.run();
 	} 
 }
---
+++ src/Square.java
@@ -0,0 +1,21 @@
+
+public class Square 
+{
+	private int alive;
+	
+	private Square neighbor1;
+	private Square neighbor2;
+	private Square neighbor3;
+	private Square neighbor4;
+	private Square neighbor5;
+	private Square neighbor6;
+	private Square neighbor7;
+	private Square neighbor8;
+	
+	public Square(int alive)
+	{
+		this.alive = alive;
+	}
+	
+	
+}
